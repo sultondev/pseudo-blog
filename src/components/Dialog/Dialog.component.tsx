90e40be88a72fb619c8aca5d07ad6eeb09ffbeb5
Sultonkhon Oblokulov <sultondeveloper@gmail.com>
2022-06-12 00:16:27 +0500

    upd: added info text

--- src/components/Dialog/Dialog.component.tsx
+++ src/components/Dialog/Dialog.component.tsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 const Dialog = () => {
-  return <div className="">Dialog</div>;
+  return <div className="">Dialog is updated</div>;
 };
 
 export const MemoDialog = memo(Dialog);
